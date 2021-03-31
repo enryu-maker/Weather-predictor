bdddc664c297f6cbb050b404f8675559d98e737b
enryu-maker <akifkhan60067@gmail.om>
2021-03-31 23:50:12 +0530

    initial

--- ml.py
+++ ml.py
@@ -1,28 +1,31 @@
+##modules##
 import pandas
 from matplotlib import pyplot 
 import sklearn
 from sklearn import linear_model
 import numpy as np
-
-
-
+############
+#loading dataset
 dataset = pandas.read_csv('data.csv')
 dataset.drop('timestamp', inplace=True, axis=1)
-#dataset.hist()
-#pyplot.show()
-#print(dataset)
+dataset.hist()
+pyplot.show()
+
+#supervised learning
 predict = 'Temperature'
 x = np.array(dataset.drop([predict], 1))
 y = np.array(dataset[predict])
 
+#trainingg the module
 x_train, x_test, y_train, y_test= sklearn.model_selection.train_test_split(x,y, test_size = 0.1)
 linear= linear_model.LinearRegression()
-
 linear.fit(x_train,y_train)
 acc= linear.score(x_test,y_test)
 print(acc*100)
 print('co',linear.coef_)
 print('intercept',linear.intercept_)
+
+#predection the values
 value=[]
 humi=int(input('Humidity==>'))
 value.append(humi)
@@ -31,5 +34,4 @@ value.append(speed)
 direction=int(input('direction==>'))
 value.append(direction)
 predication= linear.predict([value])
-
 print(predication)
